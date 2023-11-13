29b82e2638ef9ee20120300289939bf3450cf1be
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-11-13 00:38:53 -0500

    added code to ProductTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product', // This refers to the 'product' model
+        key: 'id',        // This refers to the 'id' column in the 'product' model
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',  // This refers to the 'tag' model
+        key: 'id',     // This refers to the 'id' column in the 'tag' model
+      },
+    },
   },
   {
     sequelize,
