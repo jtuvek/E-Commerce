fa8662136fd50f73cca675a3cd4711a2beb1fce5
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-11-13 00:45:03 -0500

    added code to index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,24 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
+// Products belongs to Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+// Products belong to many Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+// Tags belong to many Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
